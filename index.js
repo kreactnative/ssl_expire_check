21089c9e724c579795e7808a5bea7509f8616ba8
Narongchai Thongkongkeaw <dotnetnat@gmail.com>
2020-03-21 20:16:47 +0700

    update sites

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const sslCertificate = require("get-ssl-certificate");
 const request = require("request");
 const moment = require("moment");
+const token = "";
 
 const lineReader = require("readline").createInterface({
   input: require("fs").createReadStream("websites.txt")
@@ -12,19 +13,34 @@ lineReader.on("line", function(line) {
 
 const checkSSL = function(site) {
   sslCertificate.get(site).then(function(certificate) {
-    //console.log(certificate.valid_to);
-    const validDate = moment(certificate.valid_to, "MMM DD HH:mm:ss YYYY");
-    //console.log(validDate.format("MMM DD HH:mm:ss YYYY"));
+    console.log("check ssl ", site);
+    console.log(certificate.issuer.CN);
+    console.log(certificate.valid_to);
+    const validDate = moment(
+      certificate.valid_to,
+      "MMM DD HH:mm:ss YYYY [GMT]"
+    );
+    console.log(validDate.format("MMM DD HH:mm:ss YYYY [GMT]"));
     const currentDate = moment();
     const diffDate = validDate.diff(currentDate, "days");
-    //console.log(diffDate);
+    console.log(diffDate);
     if (diffDate <= 10) {
-      sendLine("aaa", "ssl:"+ site+" expire:"+certificate.valid_to+"   "+diffDate+" days");
+      sendLine(
+        "ssl:" +
+          site +
+          " cn:" +
+          certificate.issuer.CN +
+          " expire @ " +
+          certificate.valid_to +
+          "   " +
+          diffDate +
+          " days"
+      );
     }
   });
 };
 
-const sendLine = function(token, message) {
+const sendLine = function(message) {
   request(
     {
       method: "POST",
