1cd51cbaf33561708af14df2b6daf5ff53b9ed7b
Narongchai Thongkongkeaw <dotnetnat@gmail.com>
2020-03-21 20:06:21 +0700

    add compare date

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const sslCertificate = require("get-ssl-certificate");
 const request = require("request");
+const moment = require("moment");
 
 const lineReader = require("readline").createInterface({
   input: require("fs").createReadStream("websites.txt")
 });
 
 lineReader.on("line", function(line) {
-  console.log("Line from file:", line);
+  //console.log("Line from file:", line);
   checkSSL(line);
 });
 
 const checkSSL = function(site) {
   sslCertificate.get(site).then(function(certificate) {
-    console.log(site);
+    //console.log(site);
     //console.log(certificate.valid_from);
     console.log(certificate.valid_to);
-    sendLine("aaa", "test");
+    //sendLine("aaa", "test");
+    const validDate = moment(certificate.valid_to, "MMM DD HH:mm:ss YYYY");
+    console.log(validDate.format("MMM DD HH:mm:ss YYYY"));
+    const currentDate = moment();
+    const diffDate = validDate.diff(currentDate, "days");
+    console.log(diffDate);
   });
 };
 
@@ -34,11 +40,11 @@ const sendLine = function(token, message) {
         message: message
       }
     },
-    (err, httpResponse, body) => {
+    (err, _, body) => {
       if (err) {
         console.log(err);
       } else {
-        console.log(body);
+        //console.log(body);
       }
     }
   );
